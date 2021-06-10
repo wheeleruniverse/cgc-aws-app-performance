57288f435e68329d5f16725073c6228cf1c0b840
Justin Wheeler <wheelers_webistes@yahoo.com>
2021-06-10 00:04:33 -0400

    Terraform changes for EC2, ElastiCache, and RDS as well as updated providers/modules

--- terraform/ main.tf
+++ terraform/ main.tf
@@ -11,44 +11,87 @@ terraform {
 
 provider "aws" {
   profile = "default"
-  region  = "us-west-2"
+  region  = "us-east-1"
 }
 
-module "elasticache-redis" {
-  // https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/elasticache_cluster
-  // https://registry.terraform.io/modules/cloudposse/elasticache-redis/aws/latest
-  source  = "cloudposse/elasticache-redis/aws"
-  version = "0.39.0"
-
-  delimiter   = "-"
-  enabled     = true
-  environment = "prd"
-  name        = "redis"
-  namespace   = "CloudGuruChallenge_21.06"
-  vpc_id      = "vpc-177feb6f"
+variable "ami" {
+  default     = "ami-0aeeebd8d2ab47354" // us-east-1
+  description = "ami id"
+  type        = string
 }
 
-resource "aws_db_instance" "postgres" {
-  allocated_storage   = 10
-  engine              = "postgres"
-  engine_version      = "13.2"
-  instance_class      = "db.t3.micro"
-  name                = "postgres"
-  password            = "foobarbaz"
-  skip_final_snapshot = true
-  username            = "foo"
+variable "password" {
+  description = "password"
+  sensitive   = true
+  type        = string
+}
+
+variable "prefix" {
+  default     = "wheeler-cgc2106-"
+  description = "prefix name"
+  type        = string
+}
+
+variable "project" {
+  default     = "CloudGuruChallenge_21.06"
+  description = "project name"
+  type        = string
+}
+
+variable "type" {
+  default = {
+    cache = "cache.t3.micro"
+    db    = "db.t3.micro"
+    ec2   = "t3.micro"
+  }
+  description = "instance type"
+  type = object({
+    cache = string
+    db    = string
+    ec2   = string
+  })
+}
+
+resource "aws_db_instance" "db" {
+  allocated_storage     = 20
+  engine                = "postgres"
+  engine_version        = "13.2"
+  identifier            = "${var.prefix}db"
+  instance_class        = var.type.db
+  max_allocated_storage = 100
+  name                  = "postgres"
+  password              = var.password
+  skip_final_snapshot   = true
+  storage_type          = "gp2"
+  username              = "master"
 
   tags = {
-    Project = "CloudGuruChallenge_21.06"
+    Name    = "${var.prefix}db"
+    Project = var.project
   }
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+resource "aws_elasticache_cluster" "cache" {
+  cluster_id           = "${var.prefix}cache"
+  engine               = "redis"
+  engine_version       = "6.x"
+  node_type            = var.type.cache
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis6.x"
 
   tags = {
-    Name = "CloudGuruChallenge_21.06"
+    Name    = "${var.prefix}cache"
+    Project = var.project
+  }
+}
+
+resource "aws_instance" "server" {
+  ami           = var.ami
+  instance_type = var.type.ec2
+
+  tags = {
+    Name    = "${var.prefix}server"
+    Project = var.project
   }
 }
 
